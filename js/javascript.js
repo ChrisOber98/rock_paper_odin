1ba9a11837a7d87925ab0f2aa67b7fb293869561
Chris Ober <chrisober98@gmail.com>
2024-07-31 14:56:19 -0400

    Add game logic to determine winner and calc score

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,10 @@
-function getRandomInt(max) {
+function getRandomInt(max) 
+{
     return Math.floor(Math.random() * max);
-  }
+}
 
-function getCompMove() {
+function getCompMove() 
+{
     let move = getRandomInt(3);
 
     if (move === 0)
@@ -19,7 +21,8 @@ function getCompMove() {
     }
 }
 
-function getHumanChoice() {
+function getHumanChoice() 
+{
     let numb = Number(window.prompt("0 -> Rock, 1 -> paper, 2-> scissors: "))
 
     if (numb === 0)
@@ -36,7 +39,54 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice())
-console.log(getHumanChoice())
-console.log(getHumanChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice)
+{
+    if (humanChoice === computerChoice)
+    {
+        console.log("TIE!");
+        return;
+    }
+
+    if (humanChoice === "rock")
+    {
+        if (computerChoice === "paper")
+        {
+            computerScore++;
+            console.log("paper beats rock u lose!");
+        }
+        else
+        {
+            humanScore++;
+            console.log("rock beats scissors u win!");
+        }
+    }
+    else if (humanChoice === "paper")
+    {
+        if (computerChoice === "rock")
+        {
+            humanScore++;
+            console.log("paper beats rock u win!");
+        }
+        else
+        {
+            computerScore++;
+            console.log("scissors beats rock u lose!");
+        }
+    }
+    else
+    {
+        if (computerChoice === "rock")
+        {
+            computerScore++;
+            console.log("rock beats scissors u lose!");
+        }
+        else
+        {
+            humanScore++;
+            console.log("scissors beats paper u win!");                   
+        }
+    }
+}
+
+humanScore = 0;
+computerScore = 0;
