e1c38a3f3fb0e1cf5a7482d23c028f31f8754036
Chris Ober <chrisober98@gmail.com>
2024-07-31 14:31:35 -0400

    Add logic to compute computer's choice

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function getCompMove() {
+    let move = getRandomInt(3);
+
+    if (move === 0)
+    {
+        return "rock"
+    }
+    else if (move === 1)
+    {
+        return "paper"
+    }
+    else
+    {
+        return "scissors"
+    }
+}
+
+console.log(getCompMove());
+console.log(getCompMove());
+console.log(getCompMove());
+console.log(getCompMove());
